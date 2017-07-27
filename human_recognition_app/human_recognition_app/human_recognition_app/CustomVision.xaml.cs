3db29ca7f1c36a01d42c08ede043079867838777
dfu88 <dylan.fu@hotmail.co.nz>
2017-07-27 12:19:50 +1200

    Custom Vision API integrated

--- human_recognition_app/human_recognition_app/human_recognition_app/CustomVision.xaml.cs
+++ human_recognition_app/human_recognition_app/human_recognition_app/CustomVision.xaml.cs
@@ -1,8 +1,13 @@
-﻿using Plugin.Media;
+﻿using human_recognition_app.Model;
+using Newtonsoft.Json;
+using Plugin.Media;
 using Plugin.Media.Abstractions;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -44,7 +49,50 @@ namespace human_recognition_app
                 return file.GetStream();
             });
 
-            file.Dispose();
+            await MakePredictionRequest(file);
+        }
+
+        static byte[] GetImageAsByteArray(MediaFile file)
+        {
+            var stream = file.GetStream();
+            BinaryReader binaryReader = new BinaryReader(stream);
+            return binaryReader.ReadBytes((int)stream.Length);
+        }
+
+        async Task MakePredictionRequest(MediaFile file)
+        {
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Add("Prediction-Key", "12b7887e7e07415f9819c8000c6de358");
+
+            string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/834673c0-93ee-4109-97ba-82904517deaf/image?iterationId=ba607130-93aa-4157-add6-49c1b9d2b492";
+
+            HttpResponseMessage response;
+
+            byte[] byteData = GetImageAsByteArray(file);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(url, content);
+
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                    EvaluationModel responseModel = JsonConvert.DeserializeObject<EvaluationModel>(responseString);
+
+                    double max = responseModel.Predictions.Max(m => m.Probability);
+
+                    TagLabel.Text = (max >= 0.5) ? "This is a Human" : "This is NOT a Human";
+
+                }
+
+                //Get rid of file once we have finished using it
+                file.Dispose();
+            }
         }
     }
 } 
